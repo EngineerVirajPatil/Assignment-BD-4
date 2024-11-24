0c4f3ba7ee194187ab921404f62c821fb09e24df
EngineerVirajPatil <engineer.virajpatil@gmail.com>
2024-11-24 19:01:33 +0530

    Initial commit

---
+++ BD4_Assignment1/initDB.js
@@ -0,0 +1,37 @@
+{
+	'restaurants': [
+	  {
+	    'id': 1,
+	    'name': 'Spice Kitchen',
+	    'cuisine': 'Indian',
+	    'isVeg': 'true',
+	    'rating': 4.5,
+	    'priceForTwo': 1500,
+	    'location': 'MG Road',
+	    'hasOutdoorSeating': 'true',
+	    'isLuxury': 'false'
+	  },
+	  {
+	    'id': 2,
+	    'name': 'Olive Bistro',
+	    'cuisine': 'Italian',
+	    'isVeg': 'false',
+	    'rating': 4.2,
+	    'priceForTwo': 2000,
+	    'location': 'Jubilee Hills',
+	    'hasOutdoorSeating': 'false',
+	    'isLuxury': 'true'
+	  },
+	  {
+	    'id': 3,
+	    'name': 'Green Leaf',
+	    'cuisine': 'Chinese',
+	    'isVeg': 'true',
+	    'rating': 4.0,
+	    'priceForTwo': 1000,
+	    'location': 'Banjara Hills',
+	    'hasOutdoorSeating': 'false',
+	    'isLuxury': 'false'
+	  }
+	]
+}
